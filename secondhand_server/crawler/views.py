7fa796f71b40e982346032c30d7773e2e383191b
Ujeon <wj9304@gmail.com>
2019-09-25 21:48:51 +0900

    :sparkles: Add handle each model info router

--- secondhand_server/crawler/views.py
+++ secondhand_server/crawler/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
+from .models import Filtered_data, Average_price
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.core import serializers
+import json
+
 # Create your views here.
+def handle_each_model_info(request, brand, model):
+    average_price = Average_price.objects.filter(brand=brand, model=model)
+    filtered_data = Filtered_data.objects.filter(brand=brand, model=model)
+    result = {}
+
+    for data in average_price.values():
+        result["average_price"] = data
+
+    temp = []
+    for data in filtered_data.values():
+        temp.append(data)
+
+    result["filtered_data"] = temp
+
+    response = JsonResponse(result)
+
+    return HttpResponse(response)
+
