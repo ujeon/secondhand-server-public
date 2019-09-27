74e573ff94852044115cd84550bf62df78e8822e
KHwii <strider6k@gmail.com>
2019-09-27 14:11:56 +0900

    :sparkles: Add saving filtered data function

--- secondhand_server/crawler/urls.py
+++ secondhand_server/crawler/urls.py
@@ -3,17 +3,20 @@ from django.contrib import admin
 from django.urls import path
 
 from . import views
+from . import filter_save
 
 urlpatterns = [
-    path("eachModelInfo/<str:brand>/<str:model>/info/", views.handle_each_model_info),
+    path("eachModelInfo/<str:brand>/<str:model>/info/",
+         views.handle_each_model_info),
     path("search/", views.handle_search_price),
     path("all/", views.search_route_brand_model),
     path("allCategories/", views.get_categories),
     path("category/<str:category>/brand/", views.get_brands),
     path("category/<str:category>/brand/<str:brand>/model/", views.get_models),
+    path("input/data/", views.input_bungae_data),
+    path("input/filter/", filter_save.retrieve_raw_data)
 ]
 
 
 # 한글로 검색 시, url주소를 클라이언트에서 이스케이프해줘야 오류 발생을 막을 수 있음.
 # http://mwultong.blogspot.com/2006/10/urlencode-encoding-javascript.html
-
