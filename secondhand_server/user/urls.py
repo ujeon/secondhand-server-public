786ef6d0c050c1f98d829e8f9a5e6fd050e86182
Ujeon <wj9304@gmail.com>
2019-09-27 15:44:00 +0900

    :sparkles: Add user signup & signin and password hashing algorithm feature

--- secondhand_server/user/urls.py
+++ secondhand_server/user/urls.py
@@ -2,4 +2,7 @@ from django.urls import include, path
 
 from . import views
 
-urlpatterns = [path("signup", views.handle_signup)]
+urlpatterns = [
+    path("signup/", views.handle_user_signup),
+    path("signin/", views.handle_user_signin),
+]
